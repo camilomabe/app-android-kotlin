a3b6041e9a24f06f34d3a231fe1e0c68375b7fd3
camilomamian <camilomamian2@gmail.com>
2021-09-01 09:32:59 -0500

    Creation of login activities and connection to firebase

---
+++ app/src/main/java/com/camilomamian/finalapp/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.camilomamian.finalapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.camilomamian.finalapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
